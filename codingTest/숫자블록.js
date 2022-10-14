cf583c1b76cf72e81ef396185a8c47c6bbc7d8a5
goodsosbva <62534722+goodsosbva@users.noreply.github.com>
2022-10-14 23:59:49 +0900

    Add files via upload

---
+++ codingTest/숫자블록.js
@@ -0,0 +1,48 @@
+function solution1(begin, end) {
+    var n = end - begin + 1;
+    const answer = Array.from({length: n + 1}, () => 0);
+    
+    let number = 1;
+    while(true) {
+        let isContinue = false;
+        for (let i = 2; i < n ** n; i++) {
+            if (i * number < n + 1) {
+                isContinue = true;
+                answer[i * number] = number;
+            }
+            else {
+                break;
+            }
+        }
+        number++;
+        if (!isContinue) break;
+    }
+    
+    return answer.splice(1, n + 1);
+}
+
+function solution (begin, end) {
+    const arr = new Array(end - begin + 1).fill(0);
+    
+    for(let i = begin; i <= end; i++) {
+        arr[i - begin] = getMaxDivisor(i);
+        console.log(arr);
+    }
+    
+    // 예외 처리
+    if (begin === 1) arr[0] = 0;
+    
+    return arr;
+  }
+  
+  const getMaxDivisor = (n) => {
+    for(let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0 && n / i <= 1e7 ) {
+        return n / i;
+      }
+    }
+    return 1;
+  }
+
+// console.log(solution(1, 50));
+console.log(solution(1, 10));
