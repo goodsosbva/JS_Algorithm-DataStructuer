ed0ee62f89b975b95e8d9c87912d422818055d0a
goodsosbva <62534722+goodsosbva@users.noreply.github.com>
2024-11-14 23:53:17 +0900

    Add files via upload

--- codingTest/백준_11729_하노이탑이동순서.js
+++ codingTest/백준_11729_하노이탑이동순서.js
@@ -12,25 +12,26 @@ rl.on("line", (line) => {
 });
 
 rl.on("close", () => {
-  const result = solution(input);
-  console.log(result);
+  solution(input);
   process.exit(0);
 });
 
 function solution(input) {
   const k = parseInt(input);
-  console.log(k);
+  const result = [];
   console.log(2 ** k - 1);
-  recursive(1, 3, k);
+  recursive(1, 3, k, result);
+
+  console.log(result.join("\n"));
 }
 
-function recursive(a, b, n) {
+function recursive(a, b, n, result) {
   if (n === 1) {
-    console.log(a + " " + b);
+    result.push(`${a} ${b}`);
     return;
   }
 
-  recursive(a, 6 - a - b, n - 1);
-  console.log(a + " " + b);
-  recursive(6 - a - b, b, n - 1);
+  recursive(a, 6 - a - b, n - 1, result);
+  result.push(`${a} ${b}`);
+  recursive(6 - a - b, b, n - 1, result);
 }
